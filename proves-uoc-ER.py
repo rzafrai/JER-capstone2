a4f5133cd02543d86b9e21539f8a28c307601288
Enrique Rodríguez <enrique@datanauta.com>
2017-06-13 16:52:06 +0200

    Iniciamos clustering

--- proves-uoc-ER.py
+++ proves-uoc-ER.py
@@ -1,8 +1,12 @@
-
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 from dateutil.parser import parse
+
+from pylab import plot,show
+from numpy import vstack,array
+from numpy.random import rand
+from scipy.cluster.vq import kmeans,vq
 
 campanyes = pd.read_csv ("D:/Capstone-UOC/Data/dades-capstone-def3_2.csv", parse_dates=True)
 
@@ -10,7 +14,6 @@ campanyes.info()
 
 #validamos las columnas cargadas
 campanyes.columns
-
 
 # cálculo de la edad en años
 campanyes['data_naixement']=pd.to_datetime(campanyes['data_naixement'])
@@ -26,24 +29,62 @@ edad_media_anyos
 
 
 # recuento de usuarios por semestre
-campanyes.groupby(['semestre'])['sexe'].count()
+campanyes.groupby(['semestre'])['identif_usuari'].count()
 
 # recuento de usuarios por semestre
 campanyes.groupby(['semestre','estat_lead_recode'])['producte_comprat_recode'].count()
 
-# agrupamos por identif_usuari y calculamos count_distinct para diferentes campos
-Persones_Campanya_sexe = campanyes.groupby(['identif_usuari'])['sexe'].first()  #sexo 
-Persones_Campanya_edat = campanyes.groupby(['identif_usuari'])['edad_anyos'].mean()  #edad
-Persones_Campanya_prodcomp = campanyes.groupby(['identif_usuari'])['producte_comprat_recode'].nunique()  #producto comprado
-Persones_Campanya_puntentr = campanyes.groupby(['identif_usuari'])['punt_entrada_recode'].nunique()  #punto entrada
-Persones_Campanya_area = campanyes.groupby(['identif_usuari'])['area_prod_comprat_recode'].nunique()  #área
-Persones_Campanya_subarea = campanyes.groupby(['identif_usuari'])['subarea_prod_comprat_recode'].nunique()  #subarea
-Persones_Campanya_tipusprod = campanyes.groupby(['identif_usuari'])['tipus_producte_comprat'].nunique()  #tipo de producto
-Persones_Campanya_canal_recode = campanyes.groupby(['identif_usuari'])['canal_recode'].nunique()  #canal
-Persones_Campanya_idioma_recode = campanyes.groupby(['identif_usuari'])['idioma_recode'].nunique()  #idioma
-Persones_Campanya_semestre = campanyes.groupby(['identif_usuari'])['semestre'].nunique()  #semestre
-Persones_Campanya_regio = campanyes.groupby(['identif_usuari'])['regio'].nunique()  #región
+# agrupamos por identif_usuari y calculamos first, promedio, count_distinct para diferentes campos
+Persones_Campanya_sexe = pd.DataFrame(campanyes.groupby(['identif_usuari'])['sexe'].first())  #sexo 
+Persones_Campanya_edat = pd.DataFrame(campanyes.groupby(['identif_usuari'])['edad_anyos'].mean())  #edad
+Persones_Campanya_prodcomp = pd.DataFrame(campanyes.groupby(['identif_usuari'])['producte_comprat_recode'].nunique())  #producto comprado
+Persones_Campanya_puntentr = pd.DataFrame(campanyes.groupby(['identif_usuari'])['punt_entrada_recode'].nunique())  #punto entrada
+Persones_Campanya_area = pd.DataFrame(campanyes.groupby(['identif_usuari'])['area_prod_comprat_recode'].nunique())  #área
+Persones_Campanya_subarea = pd.DataFrame(campanyes.groupby(['identif_usuari'])['subarea_prod_comprat_recode'].nunique())  #subarea
+Persones_Campanya_tipusprod = pd.DataFrame(campanyes.groupby(['identif_usuari'])['tipus_producte_comprat'].nunique())  #tipo de producto
+Persones_Campanya_canal_recode = pd.DataFrame(campanyes.groupby(['identif_usuari'])['canal_recode'].nunique())  #canal
+Persones_Campanya_idioma_recode = pd.DataFrame(campanyes.groupby(['identif_usuari'])['idioma_recode'].nunique())  #idioma
+Persones_Campanya_semestre = pd.DataFrame(campanyes.groupby(['identif_usuari'])['semestre'].nunique())  #semestre
+Persones_Campanya_regio = pd.DataFrame(campanyes.groupby(['identif_usuari'])['regio'].nunique())  #región
 
+#añadimos índice para liberar el código de usuario que está siendo utilizado previamente como clave
+Persones_Campanya_sexe = Persones_Campanya_sexe.reset_index(drop=False)
+Persones_Campanya_edat = Persones_Campanya_edat.reset_index(drop=False)
+Persones_Campanya_prodcomp = Persones_Campanya_prodcomp.reset_index(drop=False)
+Persones_Campanya_puntentr = Persones_Campanya_puntentr.reset_index(drop=False)
+Persones_Campanya_area = Persones_Campanya_area.reset_index(drop=False)
+Persones_Campanya_subarea = Persones_Campanya_subarea.reset_index(drop=False)
+Persones_Campanya_tipusprod = Persones_Campanya_tipusprod.reset_index(drop=False)
+Persones_Campanya_canal_recode = Persones_Campanya_canal_recode.reset_index(drop=False)
+Persones_Campanya_idioma_recode = Persones_Campanya_idioma_recode.reset_index(drop=False)
+Persones_Campanya_semestre = Persones_Campanya_semestre.reset_index(drop=False)
+Persones_Campanya_regio = Persones_Campanya_regio.reset_index(drop=False)
+
+#unimos los dataframes con información por usuario a una tabla única
+Persones_activitat = pd.merge(Persones_Campanya_sexe, Persones_Campanya_edat, on='identif_usuari')
+Persones_activitat = pd.merge(Persones_activitat, Persones_Campanya_prodcomp, on='identif_usuari')
+Persones_activitat = pd.merge(Persones_activitat, Persones_Campanya_puntentr, on='identif_usuari')
+Persones_activitat = pd.merge(Persones_activitat, Persones_Campanya_area, on='identif_usuari')
+Persones_activitat = pd.merge(Persones_activitat, Persones_Campanya_subarea, on='identif_usuari')
+Persones_activitat = pd.merge(Persones_activitat, Persones_Campanya_tipusprod, on='identif_usuari')
+Persones_activitat = pd.merge(Persones_activitat, Persones_Campanya_canal_recode, on='identif_usuari')
+Persones_activitat = pd.merge(Persones_activitat, Persones_Campanya_idioma_recode, on='identif_usuari')
+Persones_activitat = pd.merge(Persones_activitat, Persones_Campanya_regio, on='identif_usuari')
+
+Persones_activitat.describe() #descriptivas de los índices obtenidos
+
+#Borramos las tablas intermedias
+del Persones_Campanya_sexe
+del Persones_Campanya_edat
+del Persones_Campanya_prodcomp
+del Persones_Campanya_puntentr
+del Persones_Campanya_area
+del Persones_Campanya_subarea
+del Persones_Campanya_tipusprod
+del Persones_Campanya_canal_recode
+del Persones_Campanya_idioma_recode
+del Persones_Campanya_semestre
+del Persones_Campanya_regio
 
 
 # filtramos campaña 20151, 20152 y 20161
@@ -66,6 +107,26 @@ evol_campanya_20161=campanya_20161.groupby(['data_ini_lead_date'])['producte_com
 evol_campanya_20151.plot()
 evol_campanya_20152.plot()
 evol_campanya_20161.plot()
+
+
+
+
+# clustering
+data = Persones_activitat.ix[:,3:11]
+
+# computing K-Means with K = 2 (2 clusters)
+centroids,_ = kmeans(data,10)
+# assign each sample to a cluster
+idx,_ = vq(data,centroids)
+
+# some plotting using numpy's logical indexing
+plot(data[idx==0,0],data[idx==0,1],'ob',
+     data[idx==1,0],data[idx==1,1],'or')
+plot(centroids[:,0],centroids[:,1],'sg',markersize=8)
+show()
+
+
+
 
 
 
